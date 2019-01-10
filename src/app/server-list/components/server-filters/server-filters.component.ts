c31da577bf877c0871caab7f15f5a4f3ba1b9732
Matteo Crosta <mr.frag85@gmail.com>
2019-01-10 01:15:53 +0100

    Implemented ram filter

--- src/app/server-list/components/server-filters/server-filters.component.ts
+++ src/app/server-list/components/server-filters/server-filters.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {FiltersOptions, ServerListState} from '../../store/server-list.state';
 
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {filter} from 'rxjs/operators';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {filter, take} from 'rxjs/operators';
 import {ApplyFilters} from '../../store/server-list.action';
 import {ApplyFiltersParameters} from '../../../core/api/server/server-collection.class';
 
@@ -25,24 +25,44 @@ export class ServerFiltersComponent implements OnInit {
 
     ngOnInit() {
         this.filtersForm = this.fb.group({
+            ram: this.fb.array([]),
+            hdd: [null],
             location: [null],
-            hdd: [null]
         });
 
         this.filtersForm.valueChanges.subscribe((val: ApplyFiltersParameters) => {
             console.log(val);
+
+            // Format RAM filters values to be usable (from true/false to selected sizes)
+            val.ram = val.ram.reduce((acc, checked, idx) => {
+                if (checked) {
+                    acc.push(this.options.ram[idx]);
+                }
+                return acc;
+            }, []);
+            if (val.ram.length === 0) {
+                val.ram = null; // I must convert it to NULL or I will filter out everything!
+            }
+
             this.store.dispatch(new ApplyFilters(val));
         });
 
         this.store
             .select(state => state['serverList'])
             .pipe(
-                filter((state: ServerListState) => !!state.filtersOptions)
+                filter((state: ServerListState) => !!state.filtersOptions),
+                take(1)
             )
             .subscribe((state: ServerListState) => {
                 console.log(state);
-
                 this.options = state.filtersOptions;
+
+                // Add ram controls to filtersForm.ram FormArray
+                const fa = this.filtersForm.get('ram') as FormArray;
+                state.filtersOptions.ram.forEach((v: number) => {
+                    fa.push(this.fb.control(false));
+                });
+
                 this.ready = true;
             });
     }
