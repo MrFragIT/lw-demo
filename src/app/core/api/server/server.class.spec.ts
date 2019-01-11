74c1d6c926b1d65405aaba20a84267db71536f4f
Matteo Crosta <matteo.crosta@edenviaggi.it>
2019-01-11 12:36:24 +0100

    Added new tests to server.class.spec

--- src/app/core/api/server/server.class.spec.ts
+++ src/app/core/api/server/server.class.spec.ts
@@ -2,14 +2,16 @@
 import {GetServerApiResponseInterface, ServerApiInterface} from './servers-api.interface';
 import {Server} from './server.class';
 
-const validData: GetServerApiResponseInterface = JSON.parse(`{"servers":[{"model":"Dell R210Intel Xeon X3440","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"2","count":"2","unit":"TB","type":"SATA2"},"location":"AmsterdamAMS-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":4999}}]}`);
+const validData4TB: GetServerApiResponseInterface = JSON.parse(`{"servers":[{"model":"Dell R210Intel Xeon X3440","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"2","count":"2","unit":"TB","type":"SATA2"},"location":"AmsterdamAMS-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":4999}}]}`);
+const validData500GB: GetServerApiResponseInterface = JSON.parse(`{"servers":[{"model":"Dell R210Intel Xeon X3440","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"500","count":"1","unit":"GB","type":"SATA2"},"location":"AmsterdamAMS-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":4999}}]}`);
+const invalidData: GetServerApiResponseInterface = JSON.parse(`{"servers":[{"model":"Dell R210Intel Xeon X3440","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"2","count":"2","unit":"XX","type":"SATA2"},"location":"AmsterdamAMS-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":4999}}]}`);
 
 describe('ServerClass', () => {
+    // Check that every attribute is as expected
     it('should properly convert inputs', () => {
-        const validInput: ServerApiInterface = validData.servers[0];
+        const validInput: ServerApiInterface = validData4TB.servers[0];
         const testClass = new Server(validInput);
 
-        // Check that every attribute is as expected
         expect(testClass.model).toEqual('Dell R210Intel Xeon X3440');
         expect(testClass.location).toEqual('AmsterdamAMS-01');
         expect(testClass.ram.memory).toEqual(16);
@@ -22,5 +24,26 @@ describe('ServerClass', () => {
         expect(testClass.price.currency).toEqual('EUR');
         expect(testClass.price.currencySymbol).toEqual('€');
         expect(testClass.price.amountCents).toEqual(4999);
+    });
+
+    it ('should convert TB to GB', () => {
+        const validInput: ServerApiInterface = validData4TB.servers[0];
+        const testClass = new Server(validInput);
+
+        expect(testClass.getTotalStorageGB()).toBe(4096);
+    });
+
+    it ('should not convert GB', () => {
+        const validInput: ServerApiInterface = validData500GB.servers[0];
+        const testClass = new Server(validInput);
+
+        expect(testClass.getTotalStorageGB()).toBe(500);
+    });
+
+    it ('should raise an exception if storage size is unknown', () => {
+        const validInput: ServerApiInterface = invalidData.servers[0];
+        const testClass = new Server(validInput);
+
+        expect(() => testClass.getTotalStorageGB()).toThrow(new Error("Unknown HDD unit XX"));
     });
 });
