851b619a91d8bf43b7a4107e1b6ebb77ea629865
Matteo Crosta <mr.frag85@gmail.com>
2019-01-12 22:13:35 +0100

    tested server-filters.component & srever-list.component

--- src/app/server-list/components/server-filters/server-filters.component.spec.ts
+++ src/app/server-list/components/server-filters/server-filters.component.spec.ts
@@ -1,26 +1,86 @@
-// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-//
-// import {ServerFiltersComponent} from './server-filters.component';
-// import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-//
-// describe('ServerFiltersComponent', () => {
-//     let component: ServerFiltersComponent;
-//     let fixture: ComponentFixture<ServerFiltersComponent>;
-//
-//     beforeEach(async(() => {
-//         TestBed.configureTestingModule({
-//             imports: [FormsModule, ReactiveFormsModule],
-//             declarations: [ServerFiltersComponent]
-//         }).compileComponents();
-//     }));
-//
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(ServerFiltersComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
-//
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+/* tslint:disable:max-line-length */
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ServerFiltersComponent} from './server-filters.component';
+import {FormArray, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Ng5SliderModule} from 'ng5-slider';
+import {Store, StoreModule} from '@ngrx/store';
+import {ServerListReducer} from '../../store/server-list.reducer';
+import {GetServerApiResponseInterface} from '../../../core/api/server/servers-api.interface';
+import {Server} from '../../../core/api/server/server.class';
+import {ServerCollection} from '../../../core/api/server/server-collection.class';
+import {ApplyFilters, LoadServersSuccessAction} from '../../store/server-list.action';
+import {ServerListState} from '../../store/server-list.state';
+
+const validResponse: GetServerApiResponseInterface = JSON.parse(`{"servers":[
+    {"model":"Test1","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"1","count":"512","unit":"GB","type":"SSD"},"location":"LOC-03","price":{"currency":"EUR","currencySymbol":"€","amountCents":1000}},
+    {"model":"Test2","ram":{"memory":"32","unit":"GB","type":"DDR3"},"hdd":{"memory":"2","count":"1","unit":"TB","type":"SATA2"},"location":"LOC-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":2000}},
+    {"model":"Test3","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"2","count":"2","unit":"TB","type":"SATA2"},"location":"LOC-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":3000}},
+    {"model":"Test4","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"1","count":"1","unit":"TB","type":"SSD"},"location":"LOC-02","price":{"currency":"EUR","currencySymbol":"€","amountCents":4000}},
+    {"model":"Test5","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"1","count":"2","unit":"TB","type":"SSD"},"location":"LOC-03","price":{"currency":"EUR","currencySymbol":"€","amountCents":5000}}
+]}`);
+
+describe('ServerFiltersComponent', () => {
+    let component: ServerFiltersComponent;
+    let fixture: ComponentFixture<ServerFiltersComponent>;
+    let store: Store<ServerListState>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                FormsModule,
+                ReactiveFormsModule,
+                Ng5SliderModule,
+                StoreModule.forRoot({
+                    serverList: ServerListReducer
+                })
+            ],
+            declarations: [ServerFiltersComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        store = TestBed.get(Store);
+        spyOn(store, 'dispatch').and.callThrough();
+        fixture = TestBed.createComponent(ServerFiltersComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('upon creation, ready state should be false', () => {
+        expect(component.ready).toEqual(false);
+    });
+
+    it('should set-up filters when filterOptions are available in state', () => {
+        const servers = validResponse.servers.map(s => new Server(s));
+        const collection = new ServerCollection(servers);
+
+        store.dispatch(new LoadServersSuccessAction(collection));
+        expect(component.filtersOptions).toBeTruthy();
+        expect((component.filtersForm.get('ram') as FormArray).length).toEqual(2);
+        expect(component.ready).toEqual(true);
+    });
+
+    it('should dispatch ApplyFilters witch correct values when filters are set', () => {
+        const expectedPayload = {
+            storage: {min: 100, max: 200},
+            ram: [32],
+            hdd: 'SSD',
+            location: 'LOC-02'
+        };
+        const action = new ApplyFilters(expectedPayload);
+        const servers = validResponse.servers.map(s => new Server(s));
+        const collection = new ServerCollection(servers);
+
+        store.dispatch(new LoadServersSuccessAction(collection));
+        component.filtersForm.get('storage').patchValue([100, 200]);
+        component.filtersForm.get('ram').patchValue([false, true]);
+        component.filtersForm.get('hdd').patchValue('SSD');
+        component.filtersForm.get('location').patchValue('LOC-02');
+        expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
+});
--- src/app/server-list/components/server-list/server-list.component.spec.ts
+++ src/app/server-list/components/server-list/server-list.component.spec.ts
@@ -1,25 +1,80 @@
-// import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-// import {ServerListComponent} from './server-list.component';
-// import {NO_ERRORS_SCHEMA} from '@angular/core';
-//
-// describe('ServerListComponent', () => {
-//     let component: ServerListComponent;
-//     let fixture: ComponentFixture<ServerListComponent>;
-//
-//     beforeEach(async(() => {
-//         TestBed.configureTestingModule({
-//             declarations: [ServerListComponent],
-//             schemas: [ NO_ERRORS_SCHEMA ]
-//         }).compileComponents();
-//     }));
-//
-//     beforeEach(() => {
-//         fixture = TestBed.createComponent(ServerListComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
-//
-//     it('should create', () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+/* tslint:disable:max-line-length */
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ServerListComponent} from './server-list.component';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {Store, StoreModule} from '@ngrx/store';
+import {ServerListReducer} from '../../store/server-list.reducer';
+import {ServerListState} from '../../store/server-list.state';
+import {LoadServersAction, LoadServersFailureAction, LoadServersSuccessAction} from '../../store/server-list.action';
+import {GetServerApiResponseInterface} from '../../../core/api/server/servers-api.interface';
+import {ServerCollection} from '../../../core/api/server/server-collection.class';
+import {Server} from '../../../core/api/server/server.class';
+
+const validResponse: GetServerApiResponseInterface = JSON.parse(`{"servers":[
+    {"model":"Test1","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"1","count":"512","unit":"GB","type":"SSD"},"location":"LOC-03","price":{"currency":"EUR","currencySymbol":"€","amountCents":1000}},
+    {"model":"Test2","ram":{"memory":"32","unit":"GB","type":"DDR3"},"hdd":{"memory":"2","count":"1","unit":"TB","type":"SATA2"},"location":"LOC-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":2000}},
+    {"model":"Test3","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"2","count":"2","unit":"TB","type":"SATA2"},"location":"LOC-01","price":{"currency":"EUR","currencySymbol":"€","amountCents":3000}},
+    {"model":"Test4","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"1","count":"1","unit":"TB","type":"SSD"},"location":"LOC-02","price":{"currency":"EUR","currencySymbol":"€","amountCents":4000}},
+    {"model":"Test5","ram":{"memory":"16","unit":"GB","type":"DDR3"},"hdd":{"memory":"1","count":"2","unit":"TB","type":"SSD"},"location":"LOC-03","price":{"currency":"EUR","currencySymbol":"€","amountCents":5000}}
+]}`);
+
+describe('ServerListComponent', () => {
+    let component: ServerListComponent;
+    let fixture: ComponentFixture<ServerListComponent>;
+    let store: Store<ServerListState>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                StoreModule.forRoot({
+                    serverList: ServerListReducer
+                })
+            ],
+            declarations: [ServerListComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        store = TestBed.get(Store);
+        spyOn(store, 'dispatch').and.callThrough();
+        fixture = TestBed.createComponent(ServerListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should dispatch LoadServers action when created', () => {
+        const action = new LoadServersAction();
+        expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
+
+    it('should find needed fields after a LoadServerSuccess action', () => {
+        const servers = validResponse.servers.map(s => new Server(s));
+        const collection = new ServerCollection(servers);
+
+        store.dispatch(new LoadServersSuccessAction(collection));
+        component.state$.subscribe(data => {
+            expect(data.servers).toEqual(jasmine.any(ServerCollection));
+            expect(data.servers.length).toEqual(5);
+            expect(data.serversCount).toEqual(5);
+            expect(data.filteredServersCount).toEqual(5);
+            expect(data.filtersOptions).toBeDefined();
+            expect(data.filtersValues).toBeDefined();
+        });
+    });
+
+    it('should find needed fields from a LoadServerFailure action', () => {
+        const reason = 'You shall not pass!!!';
+
+        store.dispatch(new LoadServersFailureAction({reason}));
+        component.state$.subscribe(data => {
+            expect(data.servers).toBeUndefined();
+            expect(data.errorMessage).toEqual(reason);
+        });
+
+    });
+});
