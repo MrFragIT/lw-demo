3c1e43ad0619e4897d8af456afa172f86921f5c7
Matteo Crosta <mr.frag85@gmail.com>
2019-01-12 17:39:37 +0100

    amountCents tested

--- src/app/shared/pipes/amountCents/amountCents.pipe.spec.ts
+++ src/app/shared/pipes/amountCents/amountCents.pipe.spec.ts
@@ -1,8 +1,24 @@
 import {AmountCentsPipe} from './amountCents.pipe';
 
 describe('AmountCentsPipe', () => {
+    let pipe: AmountCentsPipe;
+
+    beforeEach(() => {
+        pipe = new AmountCentsPipe();
+    });
+
     it('create an instance', () => {
-        const pipe = new AmountCentsPipe();
         expect(pipe).toBeTruthy();
+    });
+
+    it('should convert an integer number', () => {
+        expect(pipe.transform(4999)).toEqual(49.99);
+        expect(pipe.transform(99)).toEqual(0.99);
+        expect(pipe.transform(-4999)).toEqual(-49.99);
+        expect(pipe.transform(-99)).toEqual(-0.99);
+    });
+
+    it('should not convert a floating number', () => {
+        expect(pipe.transform(49.99)).toEqual(49.99);
     });
 });
