8dd642fd7e7c6418ac1e3ac2f521d7b101275682
Matteo Crosta <mr.frag85@gmail.com>
2019-01-13 16:39:12 +0100

    optimizations

--- src/app/shared/components/error-box/error-box.component.ts
+++ src/app/shared/components/error-box/error-box.component.ts
@@ -4,10 +4,11 @@ import {Component, Input} from '@angular/core';
     selector: 'app-error-box',
     template: `
         <div class="container-center text-center">
-            <div class="big-icon"><img src="/assets/sad.svg" alt=""></div>
+            <div class="icon"><img src="assets/storm.svg" alt=""></div>
             <br>
             <span class="lead">{{message}}</span>
         </div>`,
+    styleUrls: ['./error-box.component.scss']
 })
 export class ErrorBoxComponent {
     @Input() message: string;
